9d4e3ac8a91eba73ab4e3010469a114299e6d31e
adminailab <96946812+adminailab@users.noreply.github.com>
2023-06-27 16:03:18 +0800

    implement token optimizer

---
+++ src/pages/api/process.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    let text = req.body.text;
+    let fileType = req.body.fileType;
+    // Remove all leading/trailing white space
+    text = text.trim();
+    // Replace multiple spaces with a single space
+    text = text.replace(/\s\s+/g, ' ');
+
+    if (fileType === 'js' || fileType === 'ts' || fileType === 'sol') {
+      // Replace indents/tabs with a single space
+      text = text.replace(/\t+/g, ' ');
+      // Remove unnecessary spaces around brackets
+      text = text.replace(/\s*([{}[\]()])\s*/g, '$1');
+      // Remove unnecessary spaces around operators
+      text = text.replace(/\s*([=+-/*])\s*/g, '$1');
+      // Remove unnecessary semicolons
+      text = text.replace(/;\s*([{}[\]()])/g, '$1');
+    }
+
+    // Return processed text
+    res.status(200).json({ text });
+  } else {
+    // Handle any other HTTP method
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+export default function Home() {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [fileType, setFileType] = useState('js');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const response = await fetch('/api/process', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: inputText, fileType }),
+    });
+    const data = await response.json();
+    setOutputText(data.text);
+    setIsLoading(false);
+  };
+
+  const inputTokenCount = inputText.split(/\s+/).length;
+  const outputTokenCount = outputText.split(/\s+/).length;
+  const reduction = ((inputTokenCount - outputTokenCount) / inputTokenCount) * 100;
+
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl mb-6">Input Token Optimizer</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <select value={fileType} onChange={(e) => setFileType(e.target.value)} className="w-full p-2 border rounded text-black">
+          <option value="js">JavaScript</option>
+          <option value="ts">TypeScript</option>
+          <option value="html">HTML</option>
+          <option value="css">CSS</option>
+          <option value="json">JSON</option>
+          <option value="sol">Solidity</option>
+        </select>
+        <textarea value={inputText} onChange={(e) => setInputText(e.target.value)} className="w-full p-2 border rounded text-black" rows="10" />
+        <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded">Process</button>
+      </form>
+      <p className="mt-4 white">Input token count: {inputTokenCount}</p>
+      <p className="text-white">Output token count: {outputTokenCount}</p>
+      <p className="text-white">Reduction: {reduction.toFixed(2)}%</p>
+      {isLoading && <p className="text-black">Processing...</p>}
+      <textarea value={outputText} readOnly className="w-full p-2 border rounded mt-4 text-black" rows="10" />
+    </div>
+  );
+}
